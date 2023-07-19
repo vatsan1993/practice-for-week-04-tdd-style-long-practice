1f815874bdd3d97e63c925392deca7a69ffb95f5
Srivatsan <vatsan1993@gmail.com>
2023-07-19 14:17:16 +0530

    finished phase 1

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,3 +1,15 @@
 module.exports = function reverseString(string) {
   // Your code here
+  if (typeof string !== 'string') {
+    throw new Error(
+      'This function needs a string. But a different type of data provided.'
+    );
+  }
+  let reverse = '';
+  for (let i = string.length - 1; i >= 0; i--) {
+    let char = string[i];
+    reverse += char;
+  }
+
+  return reverse;
 };
--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,18 @@
 // Your code here
+var expect = require('chai').expect;
+const reverseString = require('../problems/reverse-string.js');
+describe('reverseString', function () {
+  it('should reverse a string.', function () {
+    let string1 = 'fun';
+    let expected = 'nuf';
+
+    let actual = reverseString(string1);
+    expect(actual).to.equal(expected);
+  });
+  it('should throw and error of the data is not a string', function () {
+    let string1 = 10;
+    expect(function () {
+      reverseString(string1);
+    }).to.throw();
+  });
+});
